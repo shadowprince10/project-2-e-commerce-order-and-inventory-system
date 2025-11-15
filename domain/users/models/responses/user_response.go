f03232ac96d6b7493cf7fd8142a6ce6ff9ac5f3e
shadowprince10 <belinda.jiahong@gmail.com>
2025-11-15 23:47:50 +0700

    feat: Added context and utility helpers

--- domain/users/models/responses/user_response.go
+++ domain/users/models/responses/user_response.go
@@ -1,5 +1,12 @@
 package responses
 
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/shadowprince10/project-2-e-commerce-order-and-inventory-system/domain/users/entities"
+)
+
 // ProfileResponse output model for successful POST /register and GET /profile
 type ProfileResponse struct {
 	ID    string `json:"id"`
@@ -16,4 +23,28 @@ type LoginResponse struct {
 // ErrorResponse for standardized error output
 type ErrorResponse struct {
 	Message string `json:"message"`
+}
+
+// mapUserToResponse function converts a domain User entity to public ProfileResponse Data Transfer Object for better security, decoupling, readability, and validation.
+func mapUserToResponse(u entities.User) ProfileResponse {
+	return ProfileResponse{
+		ID:    u.ID,
+		Name:  u.Name,
+		Email: u.Email,
+		Phone: u.Phone,
+	}
+}
+
+// respondToError function writes a standardized JSON error response
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(ErrorResponse{Message: msg})
+}
+
+// respondWithJSON function writes a standardized JSON response
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(payload)
 }
